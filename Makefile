d53d68e655f636dd01f96e4d55b4a47536da2bd2
raulfpl <raulfpl@gmail.com>
2014-09-15 02:01:21 -0300

    Move 'factorial.c' to 'tests' directory

--- Makefile
+++
@@ -1,4 +0,0 @@
-all:  factorial
-
-factorial: factorial.c cam.c cam.h
-	$(CC) factorial.c cam.c -o factorial
--- tests/factorial.c
+++ tests/factorial.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "cam.h"
+#include "../cam.h"
 
 int main(int argc, char** argv){
     camInit();
